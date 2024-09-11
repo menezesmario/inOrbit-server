3c076d4049d4a95775fe8a477b8888b8e1d69166
Mário Menezes <mario082@gmail.com>
2024-09-10 22:23:04 -0300

    feat: add week summary route and clean up imports
    - Add new getWeekSummaryRoute to server.ts
    - Remove unused imports from get-pending-goals.ts:
      - Remove 'z' from zod
      - Remove createGoal function import
    - Register getWeekSummaryRoute in server.ts"

--- src/http/routes/get-pending-goals.ts
+++ src/http/routes/get-pending-goals.ts
@@ -1,6 +1,4 @@
-import { z } from 'zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { createGoal } from '../../functions/create-goal'
 import { getWeekPendingGoals } from '../../functions/get-week-pending-goals'
 
 export const getPendingGoalsRoute: FastifyPluginAsyncZod = async app => {
